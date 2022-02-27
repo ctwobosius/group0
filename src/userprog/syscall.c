7fc622b42859bb1e1b79e511fb9b712f10731f90
Calvin Wong <ctwong958@berkeley.edu>
2022-02-27 22:18:14 +0000

    Add skeleton for checking if we have valid address space/frame, refactor exit_syscall

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,6 +12,24 @@ static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
+void check_valid_frame(struct intr_frame* f) {
+  if (
+    f == NULL || // null pointer
+    !is_user_vaddr(f)) || // illegal pointer, section A.3
+    pagedir_get_page(the_page_of_f) || // invalid pointer
+    check_if_on_boundary(f) // memory lies on page boundary
+  )
+    exit_syscall(-1);
+    return;
+}
+
+void exit_syscall(int status)
+{
+  printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
+  process_exit();
+}
+
+
 static void syscall_handler(struct intr_frame* f UNUSED) {
   uint32_t* args = ((uint32_t*)f->esp);
 
@@ -26,8 +44,7 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
 
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
-    printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
-    process_exit();
+    exit_syscall(args[1]);
   } else if (args[0] == SYS_WRITE) {
     int fd = args[1];
     const void *buffer = (void *) args[2];
