e164d28265d92a3bc0b59869abd9c26d1e7234d7
Liam Anthony <liamanthony@berkeley.edu>
2022-02-27 16:10:14 -0800

    add basic arg checking to read/write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,9 +10,13 @@
 #include "filesys/file.h"
 #include <string.h>
 
+#define MAX_OPEN_FILES 128;
+
 static void syscall_handler(struct intr_frame*);
 void exit_syscall(int status);
 void check_valid_frame(struct intr_frame* f, uint32_t* args);
+bool arg_check (char* arg);
+void do_read(struct intr_frame *f UNUSED, uint32_t* args);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
@@ -31,12 +35,28 @@ void check_valid_frame(struct intr_frame* f, uint32_t* args) {
   return;
 }
 
+//returns if an arg is valid;
+bool arg_check (char* arg) {
+  return arg != NULL && is_user_vaddr(arg) && (pagedir_get_page(thread_current()->pcb->pagedir, arg) != NULL);
+}
+
+
 void exit_syscall(int status)
 {
   printf("%s: exit(%d)\n", thread_current()->pcb->process_name, status);
   process_exit();
 }
 
+
+//called from syscall_handler to actually do the reading
+void do_read(struct intr_frame *f UNUSED, uint32_t* args) {
+  if (!arg_check((char*) args[2]) || 
+      !arg_check((char*) args[2] + (size_t)args[3] )) //check if end of buffer is valid
+      {
+    f->eax = -1;
+    exit_syscall(-1);
+  }
+}
 
 static void syscall_handler(struct intr_frame* f UNUSED) {
   uint32_t* args = ((uint32_t*)f->esp);
@@ -53,11 +73,16 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
     exit_syscall(args[1]);
+  } else if (args[0] == SYS_READ) {
+    do_read(f, args);
   } else if (args[0] == SYS_WRITE) {
     int fd = args[1];
     const void *buffer = (void *) args[2];
     size_t size = (size_t) args[3];
-
+    if (!arg_check((char*) args[2])) {
+      f->eax = -1;
+      exit_syscall(-1);
+    }
     size_t buffer_len = strlen((char *) buffer);
 
     if (fd == STDOUT_FILENO) {
